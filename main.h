882502accd85aa380c8db874a1d6e748b96b89a9
Marvel-Data <marvel.bolaji@gmail.com>
2023-04-23 18:08:27 +0000

    add header file

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+int _putchar(char c);
+
+
+#endif /* MAIN_H */
